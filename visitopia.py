162516c06e427cc7b910cdce1192b53e762508a7
cgcel <cgc.elvin@outlook.com>
2022-04-03 08:50:50 +0800

    add generate_basics(), download_single()

--- visitopia.py
+++ visitopia.py
@@ -7,7 +7,7 @@ import requests
 from tqdm import tqdm
 
 url_main = 'https://shop.vistopia.com.cn/'
-url_articles = 'https://api.vistopia.com.cn/api/v1/content/article_list?api_token=null&content_id={}&api_token=null&count={}'
+url_article_list = 'https://api.vistopia.com.cn/api/v1/content/article_list?api_token=null&content_id={}&api_token=null&count={}'
 url_content_info = 'https://api.vistopia.com.cn/api/v1/content/content-show/{}?api_token=&content_channel='
 
 
@@ -34,7 +34,7 @@ class VISTOPIA():
         self.session.headers.update(self.headers)
 
     def get_content_info_from_url(self, url: str):
-        """get_content_info_from_url
+        """get content info from url
 
         Args:
             url (str): visitopia article_list url
@@ -45,8 +45,8 @@ class VISTOPIA():
         self.content_id = resp['data']['content_id']
         self.article_count = resp['data']['article_count']
 
-    def get_articles_data(self, url: str):
-        """get_articles_data
+    def get_article_list(self, url: str):
+        """get articles data
 
         Args:
             url (str): visitopia article_list url
@@ -55,29 +55,41 @@ class VISTOPIA():
             json: articles json data
         """
         self.get_content_info_from_url(url)
-        resp = self.session.get(url_articles.format(
+        resp = self.session.get(url_article_list.format(
             self.content_id, self.article_count)).json()
         # for i in resp['data']['article_list']:
         #     print(i['title'])
         return resp
 
-    def download_articles(self, url: str):
-        """download_articles
+    def generate_basics(self, url: str):
+        """generate directory and catalog including all titles of contents
 
         Args:
             url (str): visitopia article_list url
         """
-        articles_data_json = self.get_articles_data(url=url)
+        # 新建存储文件夹
         if not os.path.exists(self.title):
             os.makedirs(self.title)
+        
+        # 生成下载目录
+        pass
 
-        # 开始下载
+    def download_all(self, url: str):
+        """download all contents in article
+
+        Args:
+            url (str): visitopia article_list url
+        """
+        articles_data_json = self.get_article_list(url=url)
+
+        self.generate_basics()
 
         # 消除特殊字符
         intab = "?*/\|.:><"
         outtab = "         "
         trantab = str.maketrans(intab, outtab)
 
+        # 开始下载
         articles = articles_data_json['data']['article_list']
         for article in articles:
             mp3_title = article['title'].translate(trantab)
@@ -92,6 +104,14 @@ class VISTOPIA():
                     for data in resp.iter_content(block_size):
                         f.write(data)
                         pbar.update(len(data))
+
+    def download_single(self, article_id):
+        """_summary_
+
+        Args:
+            article_id (_type_): _description_
+        """
+        pass
 
 
 if __name__ == '__main__':
