4b9ab8bc262ace8b93bb8105c0c9fbe5b746a641
cgcel <cgc.elvin@outlook.com>
2022-04-11 21:19:31 +0800

    添加命令行参数

--- visitopia-dl.py
+++ visitopia-dl.py
@@ -1,12 +1,35 @@
 # -*- coding: utf-8 -*-
 # author: cgcel
 
+
+import getopt
+import sys
+
 from visitopia import VISTOPIA
 
 
 def main():
-    url = input("Input url: ")
-    VISTOPIA().download_articles(url=url)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hu:v", ["help", "url="])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        sys.exit(2)
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            print("""使用方法:
+    python visitopia-dl.py [options]
+参数:
+    -h | --help 获取帮助
+    -u | --url=<节目链接>""")
+            sys.exit()
+        elif o in ("-u", "--url"):
+            url = a
+            VISTOPIA().download_all(url=url)
+        else:
+            assert False, "unhandled option"
 
 
 if __name__ == '__main__':
